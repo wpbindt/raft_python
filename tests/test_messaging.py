b956cb9d0b87c547b1fbd1339d3d78a201f61be3
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2023-11-07 09:21:41 +0100

    Add test for not taking messages when down

--- tests/test_messaging.py
+++ tests/test_messaging.py
@@ -5,15 +5,22 @@ from tests.fixtures import get_cluster, create_leader_node
 
 class TestMessaging(unittest.IsolatedAsyncioTestCase):
     async def test_that_no_messages_sent_means_no_messages_returned(self) -> None:
-        nodes = {create_leader_node()}
-        cluster = await get_cluster(nodes)
+        cluster = await get_cluster({create_leader_node()})
 
         self.assertTupleEqual(await cluster.get_messages(), tuple())
 
     async def test_one_message_gets_returned(self) -> None:
-        nodes = {create_leader_node()}
-        cluster = await get_cluster(nodes)
+        cluster = await get_cluster({create_leader_node()})
 
         await cluster.send_message('Milkshake')
 
         self.assertTupleEqual(await cluster.get_messages(), ('Milkshake',))
+
+    async def test_down_nodes_do_not_take_messages(self) -> None:
+        node = create_leader_node()
+        cluster = await get_cluster({node})
+        await node.take_down()
+
+        await cluster.send_message('Milkshake')
+
+        self.assertTupleEqual(await cluster.get_messages(), tuple())
