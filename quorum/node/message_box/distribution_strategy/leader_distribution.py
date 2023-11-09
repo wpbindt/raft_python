e1770b93fe7a7694b4dab7848a7626ae896a0407
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2023-11-09 15:58:21 +0100

    Move timeout

--- quorum/node/message_box/distribution_strategy/leader_distribution.py
+++ quorum/node/message_box/distribution_strategy/leader_distribution.py
@@ -10,10 +10,13 @@ from quorum.node.node import INode, NodeIsDown
 class LeaderDistribution(DistributionStrategy[MessageType], Generic[MessageType]):
     async def distribute(self, message: MessageType, other_nodes: set[INode[MessageType]]) -> DistributionFailed | DistributionSuccessful:
         majority = (len(other_nodes | {self}) // 2) + 1
-        tasks = asyncio.as_completed([node.send_message(message) for node in other_nodes])
-        for task, _ in zip(tasks, range(majority - 1)):
+        message_sending_tasks = asyncio.as_completed(
+            {node.send_message(message) for node in other_nodes},
+            timeout=0.5,
+        )
+        for message_sent, _ in zip(message_sending_tasks, range(majority - 1)):
             try:
-                await asyncio.wait_for(task, timeout=0.5)
+                await message_sent
             except asyncio.TimeoutError:
                 return DistributionFailed()
         return DistributionSuccessful()
