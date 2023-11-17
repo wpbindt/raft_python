8ed20c6dd98f6dc10b430269b798ca273a2f016c
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2023-11-17 09:45:39 +0100

    Refactor node server tests

--- tests/test_node_server.py
+++ tests/test_node_server.py
@@ -5,6 +5,7 @@ from datetime import timedelta
 import aiohttp
 
 from quorum.cluster.configuration import ClusterConfiguration, ElectionTimeout
+from quorum.node.node import DownableNode
 from quorum.node.node_http_server import NodeServer
 from quorum.node.role.candidate import Candidate
 from quorum.node.role.leader import Leader
@@ -15,24 +16,28 @@ from tests.fixtures import create_subject_node, create_leader_node
 class TestNodeServer(unittest.IsolatedAsyncioTestCase):
     def get_cluster_configuration(
         self,
-        election_timeout: timedelta = timedelta(seconds=0.1),
+        election_timeout: timedelta,
     ) -> ClusterConfiguration:
         return ClusterConfiguration(
             election_timeout=ElectionTimeout(max_timeout=election_timeout, min_timeout=election_timeout),
             heartbeat_period=timedelta(seconds=0.01),
-
         )
+
+    async def _kill_server(self, server_task: asyncio.Task) -> None:
+        server_task.cancel()
+        await asyncio.sleep(1)
+
+    async def start_node_server(self, node: DownableNode, election_timeout: timedelta = timedelta(seconds=0.1)) -> None:
+        server = NodeServer(node, cluster_configuration=self.get_cluster_configuration(election_timeout))
+        server_task = asyncio.create_task(server.run(8080))
+        await asyncio.sleep(0.5)
+        self.addAsyncCleanup(self._kill_server, server_task)
 
     async def test_running_the_app_runs_the_node(self) -> None:
         node = create_subject_node()
-        server = NodeServer(node, cluster_configuration=self.get_cluster_configuration())
-
-        server_task = asyncio.create_task(server.run(8080))
-
-        await asyncio.sleep(0.5)
+        await self.start_node_server(node)
 
         self.assertIsInstance(node.role, (Candidate, Leader))
-        server_task.cancel()
 
     async def send_heartbeat(self, port: int) -> None:
         async with aiohttp.ClientSession() as client:
@@ -49,10 +54,7 @@ class TestNodeServer(unittest.IsolatedAsyncioTestCase):
 
     async def test_sending_heartbeat(self) -> None:
         node = create_subject_node()
-        server = NodeServer(node, cluster_configuration=self.get_cluster_configuration(election_timeout=timedelta(seconds=0.2)))
-
-        server_task = asyncio.create_task(server.run(8080))
-        await asyncio.sleep(0.5)
+        await self.start_node_server(node)
 
         async def send_many_heartbeats() -> None:
             for _ in range(100):
@@ -66,8 +68,6 @@ class TestNodeServer(unittest.IsolatedAsyncioTestCase):
         self.assertIsInstance(node.role, Subject)
 
         heartbeat_task.cancel()
-        server_task.cancel()
-        await asyncio.sleep(2)
 
     async def request_vote(self, port: int) -> bool:
         async with aiohttp.ClientSession() as client:
@@ -78,46 +78,32 @@ class TestNodeServer(unittest.IsolatedAsyncioTestCase):
 
     async def test_request_vote(self) -> None:
         node = create_subject_node()
-        server = NodeServer(node, cluster_configuration=self.get_cluster_configuration(election_timeout=timedelta(seconds=2)))
-
-        server_task = asyncio.create_task(server.run(8080))
-        await asyncio.sleep(0.5)
+        await self.start_node_server(node)
 
         vote = await self.request_vote(port=8080)
 
         self.assertTrue(vote)
-        server_task.cancel()
-        await asyncio.sleep(2)
 
     async def test_request_vote_when_already_voted(self) -> None:
         node = create_subject_node()
-        server = NodeServer(node, cluster_configuration=self.get_cluster_configuration(election_timeout=timedelta(seconds=2)))
+        await self.start_node_server(node, election_timeout=timedelta(seconds=2))
 
         await node.request_vote()
-        server_task = asyncio.create_task(server.run(8080))
-        await asyncio.sleep(0.5)
 
         vote = await self.request_vote(port=8080)
 
         self.assertFalse(vote)
-        server_task.cancel()
-        await asyncio.sleep(2)
 
     async def test_send_and_get_messages(self) -> None:
         node = create_leader_node()
-        server = NodeServer(node, cluster_configuration=self.get_cluster_configuration())
 
-        await node.request_vote()
-        server_task = asyncio.create_task(server.run(8080))
-        await asyncio.sleep(0.5)
+        await self.start_node_server(node)
 
         await self.send_message(8080, 'hi')
         await asyncio.sleep(0.5)
         messages = await self.get_messages(8080)
 
         self.assertTupleEqual(messages, ('hi',))
-        server_task.cancel()
-        await asyncio.sleep(2)
 
     async def get_messages(self, port: int) -> tuple[str, ...]:
         async with aiohttp.ClientSession() as client:
