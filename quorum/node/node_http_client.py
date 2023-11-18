921b0b49d78ef74a0daeb26da9a0ddf9f2202c9b
Wessel Bindt <wesselbindt+gitlab@gmail.com>
2023-11-18 12:42:15 +0100

    Get rid of unclosed connection errors

--- quorum/node/node_http_client.py
+++ quorum/node/node_http_client.py
@@ -7,29 +7,28 @@ from quorum.node.role.heartbeat_response import HeartbeatResponse
 class NodeHttpClient(INode[str]):
     def __init__(self, url: str) -> None:
         self._url = url
+        self._client_session = aiohttp.ClientSession()
 
     async def request_vote(self) -> bool:
-        async with aiohttp.ClientSession() as client:
-            response = await client.post(f'{self._url}/request_vote')
+        async with self._client_session.post(f'{self._url}/request_vote') as response:
             response_data = await response.json()
         return response_data['vote']
 
     async def heartbeat(self) -> HeartbeatResponse:
-        async with aiohttp.ClientSession() as client:
-            await client.post(f'{self._url}/heartbeat')
+        async with self._client_session.post(f'{self._url}/heartbeat') as response:
+            await response.json()
         return HeartbeatResponse()
 
     async def send_message(self, message: str) -> None:
-        async with aiohttp.ClientSession() as client:
-            await client.post(
-                f'{self._url}/send_message',
-                json={'message': message},
-                headers={'Content-Type': 'application/json'},
-            )
+        async with self._client_session.post(
+            f'{self._url}/send_message',
+            json={'message': message},
+            headers={'Content-Type': 'application/json'},
+        ) as response:
+            await response.json()
 
     async def get_messages(self) -> tuple[str, ...]:
-        async with aiohttp.ClientSession() as client:
-            response = await client.get(f'{self._url}/get_messages')
+        async with self._client_session.get(f'{self._url}/get_messages') as response:
             response_data = await response.json()
         return tuple(response_data['messages'])
 
